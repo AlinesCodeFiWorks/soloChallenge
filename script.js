5c8b9f9186b8c72131b289359333c2ff0d540de2
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-01-26 00:36:34 -0600

    set up JS character "database", finished setting up full HTML form, performed mminimal styling, created basic JS number generator

--- script.js
+++ script.js
@@ -17,22 +17,91 @@ FINALLY,
 I will be presented with my randomly generated password.
 Good Luck!!*/
 
+//random number generator
+function generateNumber(selection) {
+  let randomNumber = Math.floor(Math.random() * selection);
+  return randomNumber;
+}
+
+//character database
+const lowerCase = {
+  1: "a",
+  2: "b",
+  3: "c",
+  4: "d",
+  5: "e",
+  6: "f",
+  7: "g",
+  8: "h",
+  9: "i",
+  10: "j",
+  11: "k",
+  12: "l",
+  13: "m",
+  14: "n",
+  15: "o",
+  16: "p",
+  17: "q",
+  18: "r",
+  19: "s",
+  20: "t",
+  21: "u",
+  22: "v",
+  23: "w",
+  24: "x",
+  25: "y",
+  26: "z",
+};
+
 const upperCase = {
-  1: "A",
-  2: "B",
-  3: "C",
-  4: "D",
-  5: "E",
-  6: "F",
-  7: "G",
-  8: "H",
-  9: "I",
-  10: "J",
-  11: "K",
-  12: "L",
-  13: "M",
-  14: "N",
-  15: "O",
-  16: "P",
-  17: "Q",
+  27: "A",
+  28: "B",
+  29: "C",
+  30: "D",
+  31: "E",
+  32: "F",
+  33: "G",
+  34: "H",
+  35: "I",
+  36: "J",
+  37: "K",
+  38: "L",
+  39: "M",
+  40: "N",
+  41: "O",
+  42: "P",
+  43: "Q",
+  44: "R",
+  45: "S",
+  46: "T",
+  47: "U",
+  48: "V",
+  49: "W",
+  50: "X",
+  51: "Y",
+  52: "Z",
+};
+
+const num = {
+  53: 1,
+  54: 2,
+  55: 3,
+  56: 4,
+  57: 5,
+  58: 6,
+  59: 7,
+  60: 8,
+  61: 9,
+};
+
+const specialChar = {
+  62: "!",
+  63: "@",
+  64: "#",
+  65: "$",
+  66: "%",
+  67: "^",
+  68: "&",
+  69: "*",
+  70: "?",
 };
