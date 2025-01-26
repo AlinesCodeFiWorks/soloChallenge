7e176fe7f24005efb0fa044c3aaae19d0006e2aa
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-01-25 23:59:05 -0600

    1st commit

---
+++ script.js
@@ -0,0 +1,38 @@
+/*User Story: Random Password Generator
+As a USER,
+I want to be able to generate a random password based on my preferences,
+so that I can create a secure password easily.
+Acceptance Criteria:
+  WHEN I am prompted,
+  THEN I can select how many characters I want my password to be.
+  WHEN I am prompted,
+  THEN I can select if I want uppercase letters included.
+  WHEN I am prompted,
+  THEN I can select if I want numbers included.
+  WHEN I am prompted,
+  THEN I can select if I want special characters included.
+  WHEN I have finished answering the prompts,
+  THEN I can click "Submit."
+FINALLY,
+I will be presented with my randomly generated password.
+Good Luck!!*/
+
+const upperCase = {
+  1: "A",
+  2: "B",
+  3: "C",
+  4: "D",
+  5: "E",
+  6: "F",
+  7: "G",
+  8: "H",
+  9: "I",
+  10: "J",
+  11: "K",
+  12: "L",
+  13: "M",
+  14: "N",
+  15: "O",
+  16: "P",
+  17: "Q",
+};
